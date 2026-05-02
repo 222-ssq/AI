8a8d82cd6e3e7f2ccf89872e4ff01b4eeb9d8ec2
222-ssq <2616467@sookmyung.ac.kr>
2026-05-02 15:33:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const questions = [
-  { q: "질문1", answers: [{ text: "A", type: "A" }, { text: "B", type: "B" }] },
-  { q: "질문2", answers: [{ text: "A", type: "A" }, { text: "B", type: "B" }] },
-  { q: "질문3", answers: [{ text: "A", type: "A" }, { text: "B", type: "B" }] },
-  { q: "질문4", answers: [{ text: "A", type: "A" }, { text: "B", type: "B" }] },
-  { q: "질문5", answers: [{ text: "C", type: "C" }, { text: "D", type: "D" }] },
-  { q: "질문6", answers: [{ text: "C", type: "C" }, { text: "D", type: "D" }] },
-  { q: "질문7", answers: [{ text: "C", type: "C" }, { text: "D", type: "D" }] },
-  { q: "질문8", answers: [{ text: "C", type: "C" }, { text: "D", type: "D" }] },
-  { q: "질문9", answers: [{ text: "E", type: "E" }, { text: "F", type: "F" }] },
-  { q: "질문10", answers: [{ text: "E", type: "E" }, { text: "F", type: "F" }] },
-  { q: "질문11", answers: [{ text: "E", type: "E" }, { text: "F", type: "F" }] },
-  { q: "질문12", answers: [{ text: "E", type: "E" }, { text: "F", type: "F" }] }
+  { q: "질문1", answers: [{ text: "1-1", type: "A" }, { text: "1-2", type: "B" }] },
+  { q: "질문2", answers: [{ text: "2-1", type: "A" }, { text: "2-2", type: "B" }] },
+  { q: "질문3", answers: [{ text: "3-1", type: "A" }, { text: "3-2", type: "B" }] },
+  { q: "질문4", answers: [{ text: "4-1", type: "A" }, { text: "4-2", type: "B" }] },
+  { q: "질문5", answers: [{ text: "5-1", type: "C" }, { text: "5-2", type: "D" }] },
+  { q: "질문6", answers: [{ text: "6-1", type: "C" }, { text: "6-2", type: "D" }] },
+  { q: "질문7", answers: [{ text: "7-1", type: "C" }, { text: "7-2", type: "D" }] },
+  { q: "질문8", answers: [{ text: "8-1", type: "C" }, { text: "8-2", type: "D" }] },
+  { q: "질문9", answers: [{ text: "9-1", type: "E" }, { text: "9-2", type: "F" }] },
+  { q: "질문10", answers: [{ text: "10-1", type: "E" }, { text: "10-2", type: "F" }] },
+  { q: "질문11", answers: [{ text: "11-1", type: "E" }, { text: "11-2", type: "F" }] },
+  { q: "질문12", answers: [{ text: "12-1", type: "E" }, { text: "12-2", type: "F" }] }
 ];
 
 let scores = { A:0, B:0, C:0, D:0, E:0, F:0 };
